a6cd817c01b1803b7de12b1b5ed0cc1be71af0b1
SAjiki <sergioajk@hotmail.com>
2026-03-18 18:11:10 -0400

    feat: alterado o sistema de gravação do arquivo de saida

--- src/main/java/br/gov/ses/fillbpai/service/GeradorBPAiService.java
+++ src/main/java/br/gov/ses/fillbpai/service/GeradorBPAiService.java
@@ -14,6 +14,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.nio.charset.StandardCharsets;
 
 /**
  * ============================================================
@@ -89,7 +90,9 @@ public class GeradorBPAiService {
                 return;
 
             try (BufferedWriter writer =
-                         Files.newBufferedWriter(file.toPath())) {
+                         Files.newBufferedWriter(
+                                 file.toPath(),
+                                 StandardCharsets.ISO_8859_1)) {
 
                 /**
                  * HEADER — linha 1
